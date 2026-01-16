44161a7d2f6756f96b250e81f1583fb06afca03d
Maher <nmaher121110@gmail.com>
2026-01-16 13:37:28 -0500

    Added Color

--- WithOrWithoutMarinar.py
+++ WithOrWithoutMarinar.py
@@ -1,24 +1,31 @@
-#Welcome Branch
+# Welcome Branch
 
-#Libraries Imported Here
+# Libraries Imported Here
 import sys
 import time
 
-print("\nWelcome Branch - Developer: Natalie Maher\n")
-print("Welcome to InfoTech Center v.1.0\n")
+# ANSI color codes
+CYAN = "\033[96m"
+YELLOW = "\033[93m"
+GREEN = "\033[92m"
+RESET = "\033[0m"
 
+print(f"\n{CYAN}Welcome Branch - Developer: Natalie Maher{RESET}\n")
+print(f"{CYAN}Welcome to InfoTech Center v.1.0{RESET}\n")
 
 x = 0
 ellipsis = 0
 
 while x != 20:
     x += 1
-    ellipsisMessage = ("InfoTechCenter OS Booting" + "." * ellipsis)
+    ellipsisMessage = f"{YELLOW}InfoTechCenter OS Booting{'.' * ellipsis}{RESET}"
     ellipsis += 1
     sys.stdout.write("\r\033[K" + ellipsisMessage)
     sys.stdout.flush()
-    time.sleep(.5)
+    time.sleep(0.5)
+
     if ellipsis == 4:
         ellipsis = 0
+
     if x == 20:
-        print("\nOperating System Booted Up - Retina Scanned - Access Granted")
+        print(f"\n{GREEN}Operating System Booted Up - Retina Scanned - Access Granted{RESET}")
