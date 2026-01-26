c7d1202cf5574f4667daece0b6e0adb895742bb6
Maher <nmaher121110@gmail.com>
2026-01-26 13:49:01 -0500

    Random Weather Function - Calls current weather - Stable

--- WithOrWithoutMarinar.py
+++ WithOrWithoutMarinar.py
@@ -1,1 +1,17 @@
 #Weather Branch
+
+import random
+
+weather_conditions = [
+    "Sunny",
+    "Rainy",
+    "Windy",
+    "Icy",
+    "Cloudy",
+    "Stormy",
+    "Snowy"
+]
+
+today_weather = random.choice(weather_conditions)
+
+print(f"Today's weather is {today_weather}.")
